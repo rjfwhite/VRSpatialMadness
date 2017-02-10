8a01c0c076d57925faa1113e4fb97c2aade091a4
Rob Whitehead <rob@improbable.io>
2017-02-10 22:38:47 +0000

    Adding player gen code

--- workers/unity/Assets/EntityTemplates/EntityTemplateFactory.cs
+++ workers/unity/Assets/EntityTemplates/EntityTemplateFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Improbable.General;
 using Improbable.Math;
 using Improbable.Player;
+using Improbable.Unity.Core;
 using Improbable.Unity.Core.Acls;
 using Improbable.Worker;
 
@@ -50,6 +51,25 @@ namespace Assets.EntityTemplates
             var acl = Acl.Build()
                 .SetReadAccess(CommonPredicates.PhysicsOrVisual)
                 .SetWriteAccess<Position>(CommonPredicates.PhysicsOnly)
+                .SetWriteAccess<Colour>(CommonPredicates.PhysicsOnly);
+
+            entity.SetAcl(acl);
+
+            return entity;
+        }
+
+        public static SnapshotEntity Player(Coordinates position)
+        {
+            var entity = new SnapshotEntity { Prefab = "Player" };
+
+            entity.Add(new Position.Data(position));
+            entity.Add(new Colour.Data(new Vector3f(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value)));
+            entity.Add(new VivePlayer.Data(new ViveTransform(), new ViveTransform(), new ViveTransform()));
+
+            var acl = Acl.Build()
+                .SetReadAccess(CommonPredicates.PhysicsOrVisual)
+                .SetWriteAccess<Position>(CommonPredicates.PhysicsOnly)
+                .SetWriteAccess<VivePlayer>(CommonPredicates.SpecificClientOnly(SpatialOS.Configuration.EngineId))
                 .SetWriteAccess<Colour>(CommonPredicates.PhysicsOnly);
 
             entity.SetAcl(acl);
