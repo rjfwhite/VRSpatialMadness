c4e75ac25d516582af61bbc84aba8c0850268286
Jonas Hou <jonas@improbable.io>
2017-02-11 20:31:01 +0000

    added teams

--- workers/unity/Assets/Gamelogic/GameUiController.cs
+++ workers/unity/Assets/Gamelogic/GameUiController.cs
@@ -6,15 +6,40 @@ namespace Assets.Gamelogic
     public class GameUiController : MonoBehaviour
     {
         private static Text textField;
+        private static string health = "n/a";
+        private static string points = "n/a";
+        private static string team = "n/a";
 
         private void Awake()
         {
             textField = GetComponent<Text>();
         }
 
-        public static void SetText(string text)
+        public static void SetText(string h, string p, string t)
         {
-            textField.text = text;
+            health = h;
+            points = p;
+            team = t;
+        }
+
+        public static void SetHealth(string h)
+        {
+            health = h;
+        }
+
+        public static void SetPoints(string p)
+        {
+            points = p;
+        }
+
+        public static void SetTeam(string t)
+        {
+            team = t;
+        }
+
+        private void Update()
+        {
+            textField.text = "Health: " + health + "\nPoints: " + points + "\nTeam: " + team;
         }
     }
 }
---
+++ workers/unity/Assets/Gamelogic/PlayerUiVisualizer.cs
@@ -0,0 +1,24 @@
+﻿using Improbable.General;
+using Improbable.Player;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+
+namespace Assets.Gamelogic
+{
+    public class PlayerUiVisualizer : MonoBehaviour
+    {
+        [Require] private PlayerInfo.Writer playerInfoWriter;
+        [Require] private Team.Writer teamWriter;
+        
+
+        private void Update()
+        {
+            UpdateGameUi();
+        }
+
+        private void UpdateGameUi()
+        {
+            GameUiController.SetText(playerInfoWriter.Data.health.ToString(), playerInfoWriter.Data.points.ToString(), teamWriter.Data.teamId.ToString());
+        }
+    }
+}
